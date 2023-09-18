a29598b841e908653afcdb93c22ff2b2e74929c2
teppei <teppei0830.4869@gmail.com>
2023-09-18 17:11:57 +0900

    RadioButtonForm.js を修正しました。
    削除に伴う更新作業を delete.js に関数として定義して用いるようにしました。

--- src/util/delete.js
+++ src/util/delete.js
@@ -1,5 +1,19 @@
+// [value1, value2, value3, ...] という配列からある値を削除する。
 export const deleteItemFromArray = (array, x) => {
   return array.filter((item) => {
     return item !== x;
   });
+};
+
+// Rf: https://chaika.hatenablog.com/entry/2020/05/13/083000
+// { key1: value1, key2: value2, ...} というオブジェクトからある値を削除する。
+export const deleteItemFromObject = (prev, key) => {
+  const { [key]: _, ...newObj } = prev;
+  return newObj;
+};
+
+// { key1: [array1], key2: [array2], ...} というオブジェクトの配列からある値を削除する。
+export const deleteItemFromArrayInObject = (obj, key, x) => {
+  deleteItemFromArray(obj[key], x);
+  return obj;
 };
