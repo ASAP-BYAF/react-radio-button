529816a45a390334671deaf898649109669d4ff9
teppei <teppei0830.4869@gmail.com>
2023-10-22 14:27:34 +0900

    ルーティングの設定を追加しました。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import RefineRadio from "./RefineRadio";
+import Search from "./Search";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <RefineRadio />
+      <Routes>
+        <Route path="/" element={<Search />} />
+        <Route path="/register" element={<RefineRadio />} />
+        {/* <Route path="/" element={<RefineRadio />} /> */}
+      </Routes>
     </div>
   );
 }
