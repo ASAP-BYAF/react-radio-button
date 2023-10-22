d0b5494f9917d612bf1169dbafb4e17196dcad31
teppei <teppei0830.4869@gmail.com>
2023-10-22 09:16:25 +0900

    RadioButtonFormValue.js を削除しました。

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-// import RadioButtonForm from "./RadioButtonForm";
-// import RadioButtonFormValue from "./RadioButtonFormValue";
 import RefineRadio from "./RefineRadio";
 
 function App() {
   return (
     <div className="App">
       <RefineRadio />
-      {/* <RadioButtonFormValue /> */}
     </div>
   );
 }
