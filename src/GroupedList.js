e2ad449630504f7c82f4d189503a72e2e2aed250
Teppei0830 <teppei0830.4869@gmail.com>
2023-10-23 10:47:21 +0900

    データのグループ化をバック炎で行うようにしました。

--- src/GroupedList.js
+++ src/GroupedList.js
@@ -1,38 +1,25 @@
 const makeGroupedList = (data) => {
-  const groupedData = {};
-  data.forEach((item) => {
-    const { vol_num, file_num } = item;
-    if (!groupedData[vol_num]) {
-      groupedData[vol_num] = {};
-    }
-    if (!groupedData[vol_num][file_num]) {
-      groupedData[vol_num][file_num] = [];
-    }
-    groupedData[vol_num][file_num].push(item);
-  });
-
   // マップしてJSX生成
-  const listItems = Object.entries(groupedData).map(
-    ([volNum, fileNumGroup]) => (
-      <li key={volNum}>
-        {`${volNum} 巻`}
-        <ul key={volNum}>
-          {Object.entries(fileNumGroup).map(([fileNum, items]) => (
-            <li key={fileNum}>
-              {`${fileNum} 話`}
-              <ul key={fileNum}>
-                {items.map((item, index) => (
-                  <li key={index}>
-                    {item.title} ({item.appearing_detail})
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </li>
-    )
-  );
+  const listItems = Object.entries(data).map(([volNum, fileNumGroup]) => (
+    <li key={volNum}>
+      {`${volNum} 巻`}
+      <ul key={volNum}>
+        {Object.entries(fileNumGroup).map(([fileNum, items]) => (
+          <li key={fileNum}>
+            {`${fileNum} 話`}
+            {`: ${items[0]["file_name"]}`}
+            <ul key={fileNum}>
+              {items.map((item, index) => (
+                <li key={index}>
+                  {item.task_title} ({item.appearing_detail_name})
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </li>
+  ));
   return listItems;
 };
 
